a7ec2ab68cdfbd949116cc4051e30f12b8871dd8
Gelčys Juozapas <juozapas.gelcys@mif.stud.vu.lt>
2022-03-24 22:12:57 +0200

    projecto

---
+++ sprangu-back/src/main/java/com/example/spranguback/SpranguBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.spranguback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpranguBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpranguBackApplication.class, args);
+	}
+
+}
