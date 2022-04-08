f6b23d0ac7d1de1db0d25ca3df688d5dbe232226
Gelčys Juozapas <juozapas.gelcys@mif.stud.vu.lt>
2022-04-08 19:12:46 +0300

    SPRAN-19 commit :)

--- sprangu-front/src/app/app-routing.module.ts
+++ sprangu-front/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {MainPageComponent} from "./pages/main/main-page/main-page.component";
-import {LoginComponent} from "./pages/user/login/login.component";
+import {MainPageComponent} from "./pages/main-page/main-page.component";
+import {LoginComponent} from "./pages/login/login.component";
 
 const routes: Routes = [
   {path: '', component: MainPageComponent},
